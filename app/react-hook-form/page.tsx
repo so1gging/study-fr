f953310f8550aa28de5c49e2079c0deabf86cc2a
김솔지 <so1gging@gmail.com>
2023-05-23 21:44:48 +0900

    [react-hook-from] quick start 예제 추가

--- app/react-hook-form/page.tsx
+++ app/react-hook-form/page.tsx
@@ -1,3 +1,46 @@
+'use client'
+
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+type FormType = {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+}
 export default function Page() {
-  return <>react hook form</>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormType>()
+
+  const onSubmit: SubmitHandler<FormType> = (data) => {
+    window.alert(`handleSubmit: ${JSON.stringify(data)}`)
+  }
+
+  console.log(watch('email'))
+
+  return (
+    <>
+      <span>{watch('email')}</span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input {...register('email', { required: true })} />
+        </div>
+        {errors.email && <div>This field is Required</div>}
+        <div>
+          <input {...register('firstName')} />
+        </div>
+        <div>
+          <input {...register('lastName')} />
+        </div>
+        <div>
+          <input type="password" {...register('password')} />
+        </div>
+        <input type="submit" />
+      </form>
+    </>
+  )
 }
