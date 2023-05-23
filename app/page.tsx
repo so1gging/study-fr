6819f21d42741f5122976373c854b6b52b48dab7
김솔지 <so1gging@gmail.com>
2023-05-23 21:30:01 +0900

    [react-hook-from] 라이브러리 추가 및 샘플 라우트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
       <h2>Root page</h2>
+      <Link href="/react-hook-form">react-hook-form</Link>
     </div>
   )
 }
